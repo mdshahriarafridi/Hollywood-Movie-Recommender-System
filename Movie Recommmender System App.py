cd1648d6378e22fa570f4e63031851caa8412bc0
MD. SHAHRIAR AFRIDI <Shahriarafridi66@gmail.com>
2024-07-03 01:12:01 +0600

    Add files via upload

---
+++ Movie Recommmender System App.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=37365cd70aed8862046563cfccdce686'.format(movie_id))
+    data = response.json()
+    return 'https://image.tmdb.org/t/p/w500/' + data['poster_path']
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_dict = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recomended_movies_posters = []
+    for i in movies_dict:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recomended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recomended_movies_posters
+
+movies_dict = pickle.load(open("movie_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+
+st.title('Hollywood Movie Recommender System')
+
+selected_movie_name = st.selectbox('Write a Hollywood Movie Name', movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+
+
+
+
